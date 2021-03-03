7210aeba29c118996f8c5bc6d0ca5d2614e71266
Deniz Dinler <dinlerd@hotmail.com>
2021-03-03 12:07:51 +0300

    JWT added, Authorization tests done

---
+++ SQLQuery1_claim_add.sql
@@ -0,0 +1,2 @@
+﻿INSERT INTO operationclaims (Name) values ('product.add');
+INSERT INTO useroperationclaims (userid,OperationClaimId) values (2003,1);
--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -1,5 +1,6 @@
 using Business.Abstract;
 using Business.Concrete;
+using Core.Utilities.IoC;
 using Core.Utilities.Security.Encryption;
 using Core.Utilities.Security.JWT;
 using DataAccess.Abstract;
@@ -7,6 +8,7 @@ using DataAccess.Concrete.EntityFramework;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -63,6 +65,8 @@ namespace WebAPI
                         IssuerSigningKey = SecurityKeyHelper.CreateSecurityKey(tokenOptions.SecurityKey)
                     };
                 });
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            ServiceTool.Create(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
